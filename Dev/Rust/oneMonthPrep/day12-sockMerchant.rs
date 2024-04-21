b47a9edc6420007785b111c8e6362156eda24ae7
krisconnect <74379994+krisconnect@users.noreply.github.com>
2024-04-21 11:23:34 +0100

    Just one today :) Handy dandy fold...

---
+++ Dev/Rust/oneMonthPrep/day12-sockMerchant.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+fn sockMerchant(n: i32, ar: &[i32]) -> i32 {
+    let mut sock_counts = HashMap::new();
+    for &color in ar {
+        *sock_counts.entry(color).or_insert(0) += 1;
+    }
+
+    sock_counts.values().fold(0, |acc, &count| acc + (count / 2)) //https://exercism.org/tracks/rust/concepts/fold
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let ar: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    let result = sockMerchant(n, &ar);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
